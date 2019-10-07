d968438bfbe71f6eece1ca2a14e7016ea1e78e83
Daguerian <choco.49400@gmail.com>
2019-10-07 17:59:51 +0200

    First Save

---
+++ Client-TCP.py
@@ -0,0 +1,52 @@
+import socket
+import sys
+import threading
+import time
+
+x = 0
+Reçu = 0
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print ('Client demarré \nConnexion au serveur')
+IPserveur = input('saisissez l\'adresse IP sur serveur: ')
+client.connect((IPserveur, 6789)) #Connexion au serveur
+
+NomClient = (socket.gethostname())		#Envoi Nom du client
+NomClient = NomClient.encode('UTF-8')
+client.send(NomClient)
+
+données = client.recv(1024)				#Reception Nom Serveur
+NomServeur = données.decode('UTF-8')
+print ('Connecté à ',NomServeur,'depuis',socket.gethostname())
+
+def Reception():
+	global x
+	print ('Lancement Thread de reception')
+	while True:
+		données = client.recv(1024)
+		Reçu = données.decode('UTF-8')
+		print (NomServeur,':',Reçu)
+		time.sleep(0.1)
+		if not Reçu:
+			print ('Erreur de reception')
+			x += 1
+			if x == 5:
+				print ('Fermeture de la connexion')
+				client.close()
+
+ThreadReception = threading.Thread(target=Reception)
+ThreadReception.start()
+
+while True:
+	Message = input('Saisissez: ')
+	if Message.lower() == 'deconnexion':
+		client.send(Message.encode('UTF-8'))
+		client.close() #Appel a la variable client ligne 7
+		print ('Deconecté.')
+		break
+	else:
+		MessageEncodé = Message.encode('utf-8')
+		n = client.send (MessageEncodé)
+		if (n!= len(Message)) or Message == (''):
+			print ('Erreur d\'envoi')
+		else:
+			print ('Message envoyé.')
---
+++ Server-TCP.py
@@ -0,0 +1,78 @@
+import socket
+import sys
+import threading
+import time
+import sys
+
+serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serveur.bind(('192.168.43.204', 6789))
+print ('Serveur hebergé sur ',socket.gethostname())
+serveur.listen(3) #3 connexions maxi
+
+x = 0
+Reçu = True
+
+def Stop():		#Re-def only arret, a integrer plus bas dans un if
+	MessageArret = ('Arret du serveur')
+	MessageArret = MessageArret.encode('UTF-8')
+	client.send(MessageArret)
+	print ('Fermeture de la Connexion avec le client')
+	client.close()
+	print ('Arret du serveur')
+	serveur.close() #Appel a la variable serveur ligne 7
+	sys.exit()
+
+
+def Reception():
+	global x
+	print ('Lancement Thread de reception')
+	while True:
+		données = client.recv(1024)
+
+		Reçu = données.decode('UTF-8')
+		if not Reçu:
+			print('Erreur de reception')
+			x += 1
+		
+		if x == 5:
+			Stop()
+		
+		if Reçu.lower() == ('deconnexion'):
+			print(NomClient,'deconecté')
+			Stop()
+			
+		else:
+			print (NomClient, ' : ', Reçu)
+
+ThreadReception = threading.Thread(target=Reception)
+
+
+
+### Demarrage Programme ###
+print ('Serveur démarré. \nEn attente de connexion...\n')
+
+client, AdresseClient = serveur.accept()  #client peux etre remplacé par Client1
+#Blocage tant que le client n'est pas connecté
+
+EnvoiNameServer = (socket.gethostname())			#Envoi Nom du serveur
+EnvoiNameServer = EnvoiNameServer.encode('UTF-8')
+client.send(EnvoiNameServer)
+
+données = client.recv(1024)							#Reception Nom du client
+NomClient = données.decode('UTF-8')
+print (NomClient,'connecté')
+ThreadReception.start()
+
+while True:
+
+	reponse = input('Saisissez: ')
+	reponseEncodée = reponse.encode('UTF-8')
+	n = client.send(reponseEncodée)
+	#if (n != len(reponseEncodée)):
+	if not n:
+		print ('Erreur d\'envoi')
+	if reponse.lower() == ('arret'):
+		Stop()
+		break
+	else:
+		print ('Envoyé.')
